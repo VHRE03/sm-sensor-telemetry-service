6ba3368f208420008073cca2bc162c5fd2766dd2
VHRE03 <vh.romero.e@gmail.com>
2026-09-21 13:15:04 -0600

    first commit

---
+++ src/main/java/com/vhre/sensor/telemetry/SmSensorTelemetryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vhre.sensor.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmSensorTelemetryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmSensorTelemetryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vhre/sensor/telemetry/SmSensorTelemetryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhre.sensor.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmSensorTelemetryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
